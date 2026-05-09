7171c69a1b5c229a2117c081078b2e17eec43d2b
Abhilasha <lingalaabhilasha2020@gmail.com>
2026-05-10 00:07:59 +0530

    after war deploy to tomcat

--- src/main/java/com/example/student/StudentApplication.java
+++ src/main/java/com/example/student/StudentApplication.java
@@ -2,12 +2,18 @@ package com.example.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StudentApplication {
+public class StudentApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(StudentApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
 	}
-
 }
