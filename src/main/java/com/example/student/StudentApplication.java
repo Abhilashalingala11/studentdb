45c988d0301c3f9135ea64f30f840b01686142d6
Abhilasha <lingalaabhilasha2020@gmail.com>
2026-05-12 10:25:06 +0530

    set main

--- src/main/java/com/example/student/StudentApplication.java
+++ src/main/java/com/example/student/StudentApplication.java
@@ -1,18 +1,10 @@
 package com.example.student;
 
-import  org.springframework.boot.SpringApplication;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StudentApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(StudentApplication.class);
-	}
+public class StudentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
